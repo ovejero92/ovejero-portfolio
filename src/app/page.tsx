2e91a9af0ab8a6e12c599ace3dc0c23d11dcb3db
GusDev <ovejero.gustavo92@gmail.com>
2026-02-24 22:42:05 -0300

    v1.0.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,24 @@ import About from '@/components/sections/About';
 import TechStack from '@/components/sections/TechStack';
 import Projects from '@/components/sections/Projects';
 import Contact from '@/components/sections/Contact';
-import Footer from '@/components/ui/Footer';
 
 export default function Home() {
   return (
-    <div>
+    <div style={{
+      height: '100vh',
+      overflowY: 'scroll',
+      scrollSnapType: 'y mandatory',
+      position: 'relative',
+      zIndex: 0           
+    }}>
       <Navbar />
-      <main>
+      <main >
         <Hero />
         <About />
         <TechStack />
         <Projects />
         <Contact />
       </main>
-      <Footer />
     </div>
   );
 }
--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
     <section id="contact" className="contact">
       <div className="container-custom" style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
         <h2 className="heading">Vamos a conectar</h2>
-        <p style={{ textAlign: 'center', fontSize: '1.2rem', marginBottom: '2rem' }}>
+        <p style={{ textAlign: 'center', fontSize: '1.7rem', marginBottom: '2rem' }}>
           Estoy siempre disponible para nuevos proyectos o colaboraciones. Contáctame y hablemos.
         </p>
         <form className="contact-form" action="https://formspree.io/f/xaykqvwb" method="POST">
@@ -17,10 +17,16 @@ export default function Contact() {
           <textarea name="message" rows={6} placeholder="Mensaje" required></textarea>
           <button type="submit" className="btn">Enviar</button>
         </form>
-        <p style={{ textAlign: 'center', fontSize: '1rem', color: '#6b7280' }}>
+        <p style={{ textAlign: 'center', fontSize: '1.7rem', color: '#6b7280', marginBottom: '2rem' }}>
           También puedes escribirme directamente a <a href="mailto:ovejero.gustavo92@gmail.com" style={{ color: '#0ef' }}>ovejero.gustavo92@gmail.com</a>
         </p>
       </div>
+    <footer className="footer">
+      <div className="container-custom">
+        <p className="derechos">&copy; 2026 Gustavo Ovejero. Todos los derechos reservados.</p>
+        <p><a href="#home" className="btn">Volver al inicio</a></p>
+      </div>
+    </footer>
     </section>
   );
 }
